debaa2fc7bbc8ea244f8f28a773f4bc26387dc4d
Ryland <rylandgrounds@gmail.com>
2021-03-11 20:32:39 -0600

    additional game logic

--- hangman.py
+++ hangman.py
@@ -8,5 +8,14 @@ def get_valid_word(words):
     return word
 
 def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
     
+    user_letter = input('Guess a Letter').upper()
+    if user_letter in alphabet: - used_letters:
+    
+    user_input = input('Pick a letter and pray!')
+    print(user_input) 
     
